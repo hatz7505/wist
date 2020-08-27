9eeaeee8a026c8e12d99dc67fd6fb6dd82d44eba
hatz7505 <harryteplow@hotmail.com>
2020-08-27 11:58:31 -0700

    homepage header styling created

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,11 +1,14 @@
 import React from "react";
+import "./Homepage.css"
 
 
 function Homepage() {
 
   return (
     <div>
-     Hey!!
+      <div className="lavendar-block"></div>
+      <h1 className="title">Wist</h1>
+      <div className="under-title">keeper of all things wish list</div>
     </div>
   );
 }
