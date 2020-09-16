8f245313ab2f7297a244a27eb0dc6ce5841bf3b9
hatz7505 <harryteplow@hotmail.com>
2020-09-16 16:13:52 -0700

    overlay animation now falling down

--- src/WistItem.js
+++ src/WistItem.js
@@ -1,16 +1,16 @@
-import React from "react";
-import {
-  Card,
-  CardImg,
-  CardBody,
-  CardTitle,
-  CardSubtitle
-} from "reactstrap";
+import React, { useState } from "react";
+import { Card, CardImg, CardBody, CardTitle, CardSubtitle } from "reactstrap";
 import "./wistItem.css";
 
 function WistItem({ item }) {
+  const [hoverState, setHoverState] = useState(false);
+
+  function handleHover() {
+    hoverState ? setHoverState(false) : setHoverState(true);
+  }
+
   return (
-    <div>
+    <div className="wist-item-container">
       <Card className="wist-item-card">
         <CardImg className="image" src={item.picture} alt="wist item" />
         <CardBody>
@@ -18,17 +18,20 @@ function WistItem({ item }) {
             {item.name} ${item.price}
           </CardTitle>
           <CardSubtitle className="wist-item-subtitle">
-              <a
-                href={item.link}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="wist-item-link"
-              >
-                Buy item here
-              </a>
+            <a
+              href={item.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="wist-item-link"
+            >
+              Buy item here
+            </a>
           </CardSubtitle>
         </CardBody>
       </Card>
+      <div className="overlay">
+        <div className="text">hiiii</div>
+      </div>
     </div>
   );
 }
